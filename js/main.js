92f65e2c1ac5a98e1bdb9617ad111bfc119738f7
Vyctao <vyctorcosta00@gmail.com>
2020-04-01 19:52:57 -0300

    GG falta só fzer os enigma

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function botao() {
+            var x;
+            var talvez=prompt("Digite a resposta")
+            if (talvez==="cesar") {
+                window.location.replace('enigma2.html','pagename','resizable,height=260,width=370')
+            }
+            else {
+                alert("RESPOSTA ERRADA")
+            }
+        }
